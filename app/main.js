f15681a1b4ce6fb4f70df1597db2fc792407dc5d
xiexiaotian <xiexiaotian9527@163.com>
2017-11-10 23:11:27 +0800

    Revert "webpack简易搭建(学习测试用：17-11-10)"
    This reverts commit 3da6025b4e6ef82a9cc35826de661fc407473c31.

--- app/main.js
+++
@@ -1,6 +0,0 @@
-require('./style/css.css');
-require('./style/less.less');
-require('./style/scss.scss');
-
-const greeter = require('./script/Greeter.js');
-document.querySelector("#root").appendChild(greeter());
